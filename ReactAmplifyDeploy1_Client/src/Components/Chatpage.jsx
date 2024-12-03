e13b85b1d5844d36c9968123451b9b206d42ee2f
Petersomond1 <petersomond@gmail.com>
2024-12-03 10:24:34 -0500

    ready to refactor project

--- ReactAmplifyDeploy1_Client/src/Components/Chatpage.jsx
+++ ReactAmplifyDeploy1_Client/src/Components/Chatpage.jsx
@@ -58,6 +58,23 @@ const Chatpage = () => {
       })
       .catch((err) => console.log(err));
   };
+//Here's the download for the two main channels of communication in the app.
+//The first query is the display channel, which the admin use to display content to the users.
+//The second query is the message channel, which individual use to send messages to the Admin or to other users.
+//The main display area "ContentImageVideoDisplay.jsx" is used to display content to the users, and is managed by the Admin. Its like the teacher's board in a classroom.
+//When the Admin uploads a file/content (that could be texts, music, emoji, video, image or a combination of any of these), the whole listing of file/content is displayed in the 
+//(contd) ContentListing.jsx area in an ascending timely order, and all users/audience can see the list of all the presentations/files/contents ever sent, with the latest being on the top of the list. 
+//THe latest presentation/file/content (or that selected by a user) is the one that is displayed by default on the centre display area "ContentImageVideoDisplay.jsx"
+//Users make their own responses in a chat thread that is linked to each of the presentation and this chat thread is displayed on the ChatsbyContent.jsx area on the right-hand side of the display view.
+//The chat board area "Chatboard.jsx" is used to send individual messages to the user and also is where the user can upload his own content/presentation/file that must first go to the admin for vetting
+//(contd) before approval for viewing or presentation to the general users, and is managed by the user. Its like the students' chat board in a classroom.
+//(contd and it is this same Chatboard.jsx that individual users send their messages either to the Admin for approval and onward to the general house or to a target grouping of users called audience or to other individual users.
+//The display channel is used to display content to the user. The content is displayed in the main content area of the chat page.
+//The message channel is used to send messages to the user. The messages are displayed in the chat board area of the chat page.
+//The user can send messages to the chat board area by typing in the message input field and clicking the send button.
+//The user can also upload files to the chat board area by clicking the upload button and selecting a file to upload.
+//The user can also send messages to the chat board area by clicking the send button.
+
 
   const { data: displayContent, isLoading: isLoadingDisplay } = useQuery({
     queryKey: ["display"],
@@ -189,7 +206,7 @@ const Chatpage = () => {
             </div>
           </section>
           <section className="footer_section">
-            <div className="footer_div">A Clarion Call to Re-Build Zion</div>
+            <div className="footer_div">A Clarion Call to Re-Build </div>
             <div className="footer_div">
               <div className="logout_middisplaydiv">
                 <div>
